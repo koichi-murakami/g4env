efd39effcf15ecb728bd414373eb94f077dda49b
Koichi Murakami <koichi.murakami@kek.jp>
2019-01-18 16:13:21 +0900

    initial commit for env

---
+++ g4env.10.1.3.sh
@@ -0,0 +1,13 @@
+#
+# set environment vars for data files
+#
+
+export G4NEUTRONHPDATA=/opt/geant4/data/10.1.3/G4NDL4.5
+export G4LEDATA=/opt/geant4/data/10.1.3/G4EMLOW6.41
+export G4LEVELGAMMADATA=/opt/geant4/data/10.1.3/PhotonEvaporation3.1
+export G4RADIOACTIVEDATA=/opt/geant4/data/10.1.3/RadioactiveDecay4.2
+export G4NEUTRONXSDATA=/opt/geant4/data/10.1.3/G4NEUTRONXS1.4
+export G4PIIDATA=/opt/geant4/data/10.1.3/G4PII1.3
+export G4REALSURFACEDATA=/opt/geant4/data/10.1.3/RealSurface1.0
+export G4SAIDXSDATA=/opt/geant4/data/10.1.3/G4SAIDDATA1.1
+export G4ABLADATA=/opt/geant4/data/10.1.3/G4ABLA3.0
---
+++ lend.sh
@@ -0,0 +1,5 @@
+#
+# set environment variables for LEND
+
+export G4LENDDATA=/home/kmura/Library/mpexs/data/lend/v1.3/neutrons.map
+export ThermalScatteringDATA=/home/kmura/Library/mpexs/data/ts
